e23024766f1d1e78fb3b477a3c9695923d549e46
Simon Hansen <sim.hansen@hotmail.com>
2016-03-16 12:02:15 +0100

    Kommentar til koden

--- Exercise9_1/file_server/file_server.cpp
+++ Exercise9_1/file_server/file_server.cpp
@@ -14,11 +14,12 @@ using namespace std;
 
 int main(int argc, char *argv[])
 {
+	// Setting up variables
 	int sock, portnumber, n, count;
 	portnumber = 9000;
 	socklen_t server_size;
 	socklen_t client_size;
-	int bufferSize = 1024;
+	int bufferSize = 256;
 	char buffer[bufferSize];
 	string fileName;
 	string file;
@@ -26,86 +27,86 @@ int main(int argc, char *argv[])
 	struct sockaddr_in serv_addr, cli_addr;
 
 
-	sock = socket(AF_INET, SOCK_DGRAM, 0); 
+	sock = socket(AF_INET, SOCK_DGRAM, 0); //Setting up socket
 	if (sock < 0 )
 	{
-		error("Error opening socket");
+		error("Error opening socket"); //Error handling of socket
 	} 	
-	server_size = sizeof(serv_addr);
-	bzero((char *) &serv_addr, server_size);
+	server_size = sizeof(serv_addr); // finding server size
+	bzero((char *) &serv_addr, server_size); // setting the serv_addr to 0
 
-	serv_addr.sin_family = AF_INET;
-    serv_addr.sin_addr.s_addr = INADDR_ANY;
-    serv_addr.sin_port = htons(portnumber);
+	serv_addr.sin_family = AF_INET; // setting family
+    serv_addr.sin_addr.s_addr = INADDR_ANY; // setting address
+    serv_addr.sin_port = htons(portnumber); // setting portnumber
 
-    if (bind(sock, (struct sockaddr *) &serv_addr, server_size) == -1)
+    if (bind(sock, (struct sockaddr *) &serv_addr, server_size) == -1) // Binding the serv_addr to the socket
     {
-    	error("Error on binding");
+    	error("Error on binding"); // Error handling of binding 
     }
 
-    client_size = sizeof(cli_addr);
+    client_size = sizeof(cli_addr); // finding the client size
 
     while(1)
     {
-    	bzero(buffer, bufferSize);
-    	n = recvfrom(sock,buffer,bufferSize,0, (struct sockaddr *) &cli_addr, &client_size);
+    	bzero(buffer, bufferSize); // Setting the buffer array to zero
+    	n = recvfrom(sock,buffer,bufferSize,0, (struct sockaddr *) &cli_addr, &client_size); // receiving message from client and storing it in buffer
     	if (n < 0)
     	{
-    		error("Error in recvform");
+    		error("Error in recvform"); // Error handling of received message
     	}
 
-    	cout << "Sending file" << endl;
+    	cout << "Sending file" << endl; // Help to know where in the code we are 
 
-		std::ifstream FileIn;
-		switch(buffer[0])
+		std::ifstream FileIn; // setting up our stream
+		switch(buffer[0]) // checking if letter is U,u,L,l. 
 		{
 			case 'U':
-			case 'u':
+			case 'u': // if u/U sending /proc/uptime
 				fileName = "/proc/uptime"; 
-				FileIn.open(fileName.c_str(), std::ios::in);
+				FileIn.open(fileName.c_str(), std::ios::in); // opening file
 				if(FileIn.is_open())
 				{
 					cout << "Sending /proc/uptime" << endl;
-					getline(FileIn, file);
+					getline(FileIn, file); // storing /proc/uptime data in string file
 				}
 				else
 				{
-					error("Could not open file");
+					error("Could not open file"); // Error Handling 
 				}
 	 			break;
 
 
-			case 'L':
+			case 'L': // if L/l sending /proc/loadavg
 			case 'l':
 				fileName = "/proc/loadavg";
-				FileIn.open(fileName.c_str(), std::ios::in);
+				FileIn.open(fileName.c_str(), std::ios::in); // opening file
 				if(FileIn.is_open())
 				{
 					cout << "Sending /proc/loadavg" << endl;
-					getline(FileIn, file);
+					getline(FileIn, file); // storing /proc/loadavg data in string file
 				}
 				else
 				{
-					error("Could not open file");
+					error("Could not open file"); // Error Handling
 				}
 	 			break;
 
 			default:
-					error("Wrong letter");
+					error("Wrong letter"); // Error Handling - shut down if not U/u or L/l
 				break;
 		}
-		n = sendto(sock, file.c_str(), file.length()+1, 0, (struct sockaddr *) &cli_addr, client_size);
+		n = sendto(sock, file.c_str(), file.length()+1, 0, (struct sockaddr *) &cli_addr, client_size); // sending file information to client. 
 	
 		if(n < 0)
 		{
-			error("Error in sendTo");
+			error("Error in sendTo"); // Error Handling
 		}
-		FileIn.close();
+		FileIn.close(); // closing our stream
 		break;
 
     }
     cout << "closing socket" << endl;
-    close(sock);
+    close(sock); // closing our socket. 
 
 }   
 
