0c1b3d8eafce9fc793fc133ad97d421b8a64874d
SimonHansen <sim.hansen@hotmail.com>
2016-05-10 10:12:24 +0200

    Server

--- TEEST_13_app/file_server/file_server.cpp
+++ TEEST_13_app/file_server/file_server.cpp
@@ -60,8 +60,8 @@ void file_server::sendFile(std::string fileName, long fileSize, Transport::Trans
 {
 	char buffer[BUFSIZE];
 	sprintf(buffer, "%ld",fileSize);
-	//transport->send(buffer, strlen(buffer)+1);
-	transport->send("123",4);
+	transport->send(buffer, strlen(buffer));
+
 
 	bzero(buffer,BUFSIZE);
 
@@ -78,7 +78,6 @@ void file_server::sendFile(std::string fileName, long fileSize, Transport::Trans
 	 		   	int count = FileIn.readsome(buffer,BUFSIZE); // Reading file 
 	 		   	if(count >= 0)
 	 		   	{
-	 		   		cout << "'" << count << "'" << endl;
 					transport->send(buffer,count); // sending file
 					rest -= count;
 	 		    }
